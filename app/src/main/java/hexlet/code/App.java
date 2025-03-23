c8ff5f26f6baad913aca76760b9c8cc46c07ee3a
Evgeniy Kanovalov <jonnypovidlos@yandex.ru>
2025-03-23 14:04:46 +0500

    Add option -f,--format Add parameters filepath1, filepah2

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,6 +2,8 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
 import java.util.concurrent.Callable;
 
@@ -12,6 +14,15 @@ public class App implements Callable<Integer> {
     public Integer call() throws Exception {
         return 0;
     }
+
+    @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
+    String format;
+
+    @Parameters(index = "0", description = "path to first file", paramLabel = "filepath1")
+    String filepath1;
+
+    @Parameters(index = "1", description = "path to second file", paramLabel = "filepath2")
+    String filepath2;
 
     public static void main(String[] args) {
         var exitCode = new CommandLine(new App()).execute(args);
