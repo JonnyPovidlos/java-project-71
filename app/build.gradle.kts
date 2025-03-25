c047b3b9bf7ef7f49c566eab9faa6e1b100f5ff8
Evgeniy Kanovalov <jonnypovidlos@yandex.ru>
2025-03-25 12:34:01 +0500

    add jacoco

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,10 +4,15 @@ plugins {
     id("org.sonarqube") version "6.0.1.5171"
     application
     checkstyle
+    jacoco
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_22
+}
 
 repositories {
     mavenCentral()
@@ -16,12 +21,21 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("info.picocli:picocli:4.7.6")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.18.3")
 }
 
 tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+    reports {
+        xml.required = true
+    }
 }
 
 application {
