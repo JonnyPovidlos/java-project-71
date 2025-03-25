9a6a2458ee6498d7b7962297deeb94062d54c4db
Evgeniy Kanovalov <jonnypovidlos@yandex.ru>
2025-03-25 11:46:32 +0500

    Differ generate with json

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -6,20 +6,26 @@ import java.util.stream.Stream;
 
 public class Differ {
     public static String generate(Map<String, Object> struct1, Map<String, Object> struct2) {
-        System.out.println(struct1);
-        System.out.println(struct2);
+
         StringBuilder result = new StringBuilder("{");
         List<String> sortedKeys = Stream.concat(struct1.keySet().stream(), struct2.keySet().stream())
                 .distinct()
                 .sorted()
                 .toList();
+
         for (String key : sortedKeys) {
-//            var contains1 = struct1.containsKey(key);
-//            var contains2 = struct2.containsKey(key);
-//            if (contains1 && !contains2) {
-//                result.append("\n  - ").append(key);
-//            }
-            result.append("\n\t").append(key);
+            if (!struct1.containsKey(key)) {
+                result.append(String.format("\n  + %s: %s", key, struct2.get(key)));
+            } else if (!struct2.containsKey(key)) {
+                result.append(String.format("\n  - %s: %s", key, struct1.get(key)));
+
+            } else if (!struct1.get(key).equals(struct2.get(key))) {
+                result.append(String.format("\n  - %s: %s", key, struct1.get(key)));
+                result.append(String.format("\n  + %s: %s", key, struct2.get(key)));
+
+            } else {
+                result.append(String.format("\n  %s: %s", key, struct1.get(key)));
+            }
         }
         result.append("\n}");
         return result.toString();
